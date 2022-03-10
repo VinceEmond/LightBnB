8909f94220e952fa508263471501d210f60bba9d
VinceEmond <vince.emond@icloud.com>
2022-03-10 21:24:10 +0000

    Implemented LighthouseBnB Reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -81,7 +81,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+      SELECT reservations.id, reservations.start_date, avg(rating) as average_rating, properties.*
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+      LIMIT 10;
+  `, [guest_id])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((error)=> {
+      console.log(error.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
